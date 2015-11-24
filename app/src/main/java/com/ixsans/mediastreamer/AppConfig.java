ad1b8a96cfab3d7e5bf9eaf8e817f7016361c43e
ikhsanudin hakim <ikhsanudinhakim@gmail.com>
2015-11-24 14:38:37 +0700

    initial commit

---
+++ app/src/main/java/com/ixsans/mediastreamer/AppConfig.java
@@ -0,0 +1,13 @@
+package com.ixsans.mediastreamer;
+
+/**
+ * Copyright (C) Yanmii - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Originally written by Author Name ikhsanudinhakim, 11/24/15
+ */
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://192.168.10.64:1935/live/mediastream";
+    public static final String PUBLISHER_USERNAME = "android";
+    public static final String PUBLISHER_PASSWORD = "teststreamer";
+}
